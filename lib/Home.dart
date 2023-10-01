53bb0d8ea3a7b1138b218db9da7c5520e1ece536
ARYANDARADE <arempire83@gmail.com>
2023-10-01 11:03:41 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	android/app/build.gradle

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,28 +1,55 @@
 import 'package:basic/NavBar.dart';
+import 'package:basic/SafeHome.dart';
+import 'package:basic/main.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_phone_direct_caller/flutter_phone_direct_caller.dart';
 import 'Chat.dart';
 import 'Report.dart';
 import 'Location.dart';
+import 'quotes.dart';
 import 'sidebar.dart';
+import 'package:flutter_sms/flutter_sms.dart';
 
 import 'Signup.dart';
 
+void main() {
+  runApp(SidebarApp());
 
-
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-  @override
-  _HomePageState createState() => _HomePageState();
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-    );
-  }
 }
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+
 class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    List<String> sweetSayings = [
+      "Your presence, lights up the whole room",
+      "We admire,Your strong personality.",
+      "We’ll help you In any way we can,",
+      "You are Strong and courageous",
+      'I say if I’m beautiful. I say if I’m strong',
+      'Above all, be the heroine of your life, not the victim',
+    ];
+
+    List<String> articleTitle = [
+      "We have to end Violance",
+      "Be a change",
+      "You are strong"
+    ];
+    List<String> imageSliders = [
+      "https://media.istockphoto.com/photos/silhouette-of-super-strong-successful-businesswoman-picture-id1249879109?k=20&m=1249879109&s=612x612&w=0&h=I-joEMjqkq1wCJZJeLWUCd1d2HcB5WxBShdkA9YM0cg=",
+      "https://media.istockphoto.com/vectors/young-woman-looks-at-the-mirror-and-sees-her-happy-reflection-vector-id1278815846?k=20&m=1278815846&s=612x612&w=0&h=JUTmV9Of-_ILOfXBfV9Cmp_41yuTliSdFIcZy5LKuss=",
+      "https://media.istockphoto.com/vectors/mental-health-or-psychology-concept-with-flowering-human-head-vector-id1268669581?k=20&m=1268669581&s=612x612&w=0&h=YVLTKCZXKugEn40aqOkir4vcoFeTUAQToa1i3AFYRNU=",
+      "https://media.istockphoto.com/photos/confidence-and-strength-concept-picture-id1086700012?k=20&m=1086700012&s=612x612&w=0&h=1wWVN3AB7BH7o3y2A2b-NG3HB9H6Dwkc9OLz2lxgwAY=",
+    ];
+
+
     final screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       extendBody: true,
@@ -44,20 +71,35 @@ class _HomePageState extends State<HomePage> {
           Positioned(
             top: 45.0,
             left: screenWidth * 0.29,
-            child: Text(
-              "HOME PAGE ",
-              style: TextStyle(
-                color: Colors.redAccent,
-                fontSize: screenWidth * 0.10,
-                fontWeight: FontWeight.bold,
-              ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Image.asset("assets/images/logo.png"),
+                Text(
+                  "Vigilance",
+                  style: TextStyle(
+                    color: Colors.redAccent,
+                    fontSize: screenWidth * 0.10,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
             ),
           ),
+      Column(
+        children: [
+          Center(child: SafeHome(),),
+
+        ],
+      ),
 
 
 
 
-          /*Positioned(
+
+
+
+      /*Positioned(
             top: 80.0, // Adjust the position as needed
             left: screenWidth * 0.65, // Adjust the position as needed
             child: Text(
@@ -75,3 +117,4 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
+
